ed14bb292b6d616788af02a9c13fa4dd08ce2ee5
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-02-08 18:40:29 +0100

    kdrive: init at version 3.5.7.20240124
    Built from extracted AppImage content (details on discourse: https://discourse.nixos.org/t/issue-running-packaging-an-appimage/36923/26).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -41849,4 +41849,8 @@ with pkgs;
   insulator2 = callPackage ../applications/misc/insulator2 {};
 
   animdl = python3Packages.callPackage ../applications/video/animdl { };
+
+  kdrive = callPackage ../by-name/kd/kdrive/package.nix {
+    inherit (qt6) wrapQtAppsHook;
+  };
 }
